b5812e519aa1c3e927fe89c7f21bcafe7655fd9c
kristiyandd <kristiyandd@gmail.com>
2016-06-08 13:09:03 +0300

    Migrate Editor Extension to IFrame (editorText)

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@
 
 define(function(require, exports, module) {
   "use strict";
-  
+
   var extensionID = "editorText"; // ID should be equal to the directory name where the ext. is located
   var extensionSupportedFileTypes = [
     "h", "c", "clj", "coffee", "coldfusion", "cpp",
@@ -18,9 +18,14 @@ define(function(require, exports, module) {
   console.log("Loading " + extensionID);
 
   var TSCORE = require("tscore");
-  var cmEditor;
+  var containerElID;
+  var currentFilePath;
+  var $containerElement;
+  var extensionsPath = TSCORE.Config.getExtensionPath();
   var extensionDirectory = TSCORE.Config.getExtensionPath() + "/" + extensionID;
+  var cmEditor;
   var contentLoaded = false;
+
 
   var filetype = [];
   filetype.h = "clike";
@@ -58,13 +63,29 @@ define(function(require, exports, module) {
 
   function init(filePath, containerElementID, isViewerMode) {
     console.log("Initalization Text Editor...");
-    contentLoaded = false;
+    containerElementID = containerElementID;
+    $containerElement = $('#' + containerElementID);
 
-    var fileExt = filePath.substring(filePath.lastIndexOf(".") + 1, filePath.length).toLowerCase();
-    
-    var $containerElement = $("#" + containerElementID); 
+    currentFilePath = filePath;
+    $containerElement.empty();
+    //$containerElement.css("background-color" , "white");
+    $containerElement.append($('<iframe>', {
+      sandbox: "allow-same-origin allow-scripts allow-modals",
+      id: "iframeViewer",
+      "nwdisable": "",
+      //"nwfaketop": "",
+      "src": extensionDirectory + "/index.html?&locale=" + TSCORE.currentLanguage,
+    }));
 
-    $containerElement.append('<div id="code" style="width: 100%; height: 100%; z-index: 0;">');
+    TSCORE.IO.loadTextFilePromise(filePath).then(function(content) {
+      setContent(content);
+      //viewerMode(isViewer);
+    }, function(error) {
+      TSCORE.hideLoadingAnimation();
+      TSCORE.showAlertDialog("Loading " + filePath + " failed.");
+      console.error("Loading file " + filePath + " failed " + error);
+    });
+
     var mode = filetype[fileExt];
     var modePath;
     if (mode) {
@@ -77,8 +98,8 @@ define(function(require, exports, module) {
       //extensionDirectory + '/libs/codemirror/addon/search/searchcursor',
       "marked",
       "text!" + extensionDirectory + '/toolbar.html',
-      modePath,                   
-      'css!' + extensionDirectory + '/libs/codemirror/lib/codemirror.css',      
+      modePath,
+      'css!' + extensionDirectory + '/libs/codemirror/lib/codemirror.css',
       'css!' + extensionDirectory + '/extension.css',
       'css!' + extensionDirectory + '/../../assets/tagspaces.css'
     ], function(CodeMirror, marked, toolbarTPL) {
@@ -86,131 +107,49 @@ define(function(require, exports, module) {
       var extUI = extUITmpl({
         id: extensionID
       });
-      $containerElement.append(extUI);      
-      
-      //platformTuning();
-      if (isCordova) {
-        TSCORE.reLayout();
-      }
-      try {
-        $('#' + extensionID + 'Container [data-i18n]').i18n();
-        $('#aboutExtensionModalEditorText').on('show.bs.modal', function() {
-          console.warn('#aboutExtensionModalEditorText click');
-          $.ajax({
-            url: extensionDirectory + '/README.md',
-            type: 'GET'
-          })
-          .done(function(mdData) {
-            //console.log("DATA: " + mdData);
-            if (marked) {
-              var modalBody = $("#aboutExtensionModalEditorText .modal-body");
-              modalBody.html(marked(mdData, { sanitize: true }));
-              handleLinks(modalBody);
-            } else {
-              console.log("markdown to html transformer not found");
-            }  
-          })
-          .fail(function(data) {
-            console.warn("Loading file failed " + data);
-          });
-        }); 
-        
-        $("#printButton").on("click", function() {          
-          $(".dropdown-menu").dropdown('toggle');
-          //window.print();
-          printContent();
-        });
-      
-        if (isCordova) {
-          $("#printButton").hide();
-        }          
-        
-      } catch (err) {
-        console.log("Failed translating extension");
-      }      
-      
-      
-      
-      var cursorBlinkRate = isViewerMode ? -1 : 530; // disabling the blinking cursor in readonly mode
-      var lineNumbers = !isViewerMode;
-      //var saveKB = convertMouseTrapToCodeMirrorKeyBindings(TSCORE.Config.getSaveDocumentKeyBinding());
-      var keys = {};
-
-      keys[convertMouseTrapToCodeMirrorKeyBindings(TSCORE.Config.getSaveDocumentKeyBinding())] = function() {
-        TSCORE.FileOpener.saveFile();
-      };
-
-      keys[convertMouseTrapToCodeMirrorKeyBindings(TSCORE.Config.getCloseViewerKeyBinding())] = function() {
-        TSCORE.FileOpener.closeFile();
-      };
-
-      //console.warn("#aboutButton: " +document.getElementById("aboutButton"));
-      //console.warn("#aboutButton: " +template);
-       
-
-      cmEditor = new CodeMirror(document.getElementById("code"), {
-        fixedGutter: false,
-        mode: mode,
-        lineNumbers: lineNumbers,
-        lineWrapping: true,
-        tabSize: 2,
-        //lineSeparator: isWin ? "\n\r" : null, // TODO check under windows if content contains \n\r -> set
-        collapseRange: true,
-        matchBrackets: true,
-        cursorBlinkRate: cursorBlinkRate,
-        readOnly: isViewerMode ? "nocursor" : isViewerMode,
-        autofocus: true,
-        //theme: "lesser-dark",
-        //extraKeys: keys // workarrounded with bindGlobal plugin for mousetrap
-      });
-
-      cmEditor.on("change", function() {
-        if (contentLoaded) {
-          TSCORE.FileOpener.setFileChanged(true);
-        }
-      });
-
-      cmEditor.setSize("100%", "100%");
-      TSCORE.IO.loadTextFilePromise(filePath).then(function(content) {
-        exports.setContent(content);
-      }, 
-      function(error) {
-        TSCORE.hideLoadingAnimation();
-        TSCORE.showAlertDialog("Loading " + filePath + " failed.");
-        console.error("Loading file " + filePath + " failed " + error);
-      });
+      $containerElement.append(extUI);
     });
-  }
 
-  function handleLinks($element) {
-    $element.find("a[href]").each(function() {
-      var currentSrc = $(this).attr("href");
-      $(this).bind('click', function(e) {
-        e.preventDefault();
-        var msg = {command: "openLinkExternally", link : currentSrc};
-        window.parent.postMessage(JSON.stringify(msg), "*");
-      });
-    });
-  }
-
-
- 
-  function printContent() {
-    var extUITmpl = Handlebars.compile(
-      '<iframe id="printerViewer" name="printerViewer" sandbox="allow-same-origin allow-scripts allow-modals" style="background-color: white; border: 0px;display:none;" class="flexMaxHeight" nwdisable="" src=""></iframe>'
-    );
-    var extUI = extUITmpl({});
-    if (window.frames.printerViewer) {       
-      document.getElementById("viewer").removeChild(document.getElementById("printerViewer"));
-    }
-    $("#viewer").append(extUI);  
-    var prtContent = document.getElementById("code");
-    var WinPrint =  window.frames.printerViewer;											
-    WinPrint.document.write("<html><head><head><body>" + prtContent.innerHTML + "</body></html>");
-    WinPrint.document.close();    
-    WinPrint.focus();		
-    WinPrint.print();		
-    document.getElementById("viewer").removeChild(document.getElementById("printerViewer"));    
+    //keys[convertMouseTrapToCodeMirrorKeyBindings(TSCORE.Config.getSaveDocumentKeyBinding())] = function() {
+    //  TSCORE.FileOpener.saveFile();
+    //};
+    //
+    //keys[convertMouseTrapToCodeMirrorKeyBindings(TSCORE.Config.getCloseViewerKeyBinding())] = function() {
+    //  TSCORE.FileOpener.closeFile();
+    //};
+    //
+    //cmEditor = new CodeMirror(document.getElementById("code"), {
+    //  fixedGutter: false,
+    //  mode: mode,
+    //  lineNumbers: lineNumbers,
+    //  lineWrapping: true,
+    //  tabSize: 2,
+    //  //lineSeparator: isWin ? "\n\r" : null, // TODO check under windows if content contains \n\r -> set
+    //  collapseRange: true,
+    //  matchBrackets: true,
+    //  cursorBlinkRate: cursorBlinkRate,
+    //  readOnly: isViewerMode ? "nocursor" : isViewerMode,
+    //  autofocus: true,
+    //  //theme: "lesser-dark",
+    //  //extraKeys: keys // workarrounded with bindGlobal plugin for mousetrap
+    //});
+    //
+    //cmEditor.on("change", function() {
+    //  if (contentLoaded) {
+    //    TSCORE.FileOpener.setFileChanged(true);
+    //  }
+    //});
+    //
+    //cmEditor.setSize("100%", "100%");
+    //TSCORE.IO.loadTextFilePromise(filePath).then(function(content) {
+    //  exports.setContent(content);
+    //},
+    //function(error) {
+    //  TSCORE.hideLoadingAnimation();
+    //  TSCORE.showAlertDialog("Loading " + filePath + " failed.");
+    //  console.error("Loading file " + filePath + " failed " + error);
+    //});
+    //});
   }
 
   // Converts mod+s to Ctrl+S
@@ -232,15 +171,30 @@ define(function(require, exports, module) {
   }
 
   function setContent(content) {
+    console.log("Extension content");
+    console.debug(content);
+    console.log("--------------");
     //console.log("Content: "+content);
     var UTF8_BOM = "\ufeff";
     if (content.indexOf(UTF8_BOM) === 0) {
       content = content.substring(1, content.length);
     }
-    cmEditor.setValue(content);
-    cmEditor.clearHistory();
-    cmEditor.refresh();
-    contentLoaded = true;
+
+    var contentWindow = document.getElementById("iframeViewer").contentWindow;
+    if (typeof contentWindow.setContent === "function") {
+      console.log(content);
+      contentWindow.setContent(content , currentFilePath);
+    } else {
+      window.setTimeout(function() {
+        if (typeof contentWindow.setContent === "function") {
+          contentWindow.setContent(content);
+        }
+      } , 500);
+    }
+    /*    cmEditor.setValue(content);
+     cmEditor.clearHistory();
+     cmEditor.refresh();
+     contentLoaded = true;*/
   }
 
   function getContent() {
@@ -254,3 +208,4 @@ define(function(require, exports, module) {
   exports.viewerMode = viewerMode;
 
 });
+
---
+++ main.js
@@ -0,0 +1,146 @@
+/* Copyright (c) 2013-2016 The TagSpaces Authors.
+ * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
+/* globals JSONEditor */
+/* globals marked */
+"use strict";
+
+var isCordova;
+var isWin;
+var isWeb;
+
+$(document).ready(function() {
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  var locale = getParameterByName("locale");
+  var $htmlContent;
+  var extSettings;
+  loadExtSettings();
+
+  isCordova = parent.isCordova;
+  isWin = parent.isWin;
+  isWeb = parent.isWeb;
+
+  $(document).on('drop dragend dragenter dragover', function(event) {
+    event.preventDefault();
+  });
+
+  $('#aboutExtensionModal').on('show.bs.modal', function() {
+    $.ajax({
+      url: 'README.md',
+      type: 'GET'
+    }).done(function(editorTextData) {
+      //console.log("DATA: " + editorTextData);
+      if (marked) {
+        var modalBody = $("#aboutExtensionModal .modal-body");
+        modalBody.html(marked(editorTextData, {sanitize: true}));
+        handleLinks(modalBody);
+      } else {
+        console.log("markdown to html transformer not found");
+      }
+    }).fail(function(data) {
+      console.warn("Loading file failed " + data);
+    });
+  });
+
+  function handleLinks($element) {
+    $element.find("a[href]").each(function() {
+      var currentSrc = $(this).attr("href");
+      $(this).bind('click', function(e) {
+        e.preventDefault();
+        var msg = {command: "openLinkExternally", link: currentSrc};
+        window.parent.postMessage(JSON.stringify(msg), "*");
+      });
+    });
+  }
+
+  $("#aboutButton").on("click", function(e) {
+    $("#aboutExtensionModal").modal({show: true});
+  });
+
+  if (isCordova) {
+    $("#printButton").hide();
+  }
+
+  // Init internationalization
+  $.i18n.init({
+    ns: {namespaces: ['ns.editorText']},
+    debug: true,
+    lng: locale,
+    fallbackLng: 'en_US'
+  }, function() {
+    $('[data-i18n]').i18n();
+  });
+
+  function loadExtSettings() {
+    extSettings = JSON.parse(localStorage.getItem("editorTextSettings"));
+  }
+
+});
+
+var editorText;
+var isViewer = true;
+var filePath;
+
+function contentChanged() {
+  //console.log('Content changed');
+  var msg = {command: "contentChangedInEditor", filepath: filePath};
+  window.parent.postMessage(JSON.stringify(msg), "*");
+}
+
+function setContent(content, filePath) {
+  if (content !== undefined || content !== null) {
+    console.log("Set Main Content");
+    console.debug(content);
+    console.log("--------------");
+  } else {
+    console.log("Undefined value");
+  }
+
+ // var cursorBlinkRate = isViewerMode ? -1 : 530; // disabling the blinking cursor in readonly mode
+  //var lineNumbers = !isViewerMode;
+  var contentLoaded = false;
+  var $htmlContent = $("#editorText");
+
+  var cmEditor = new CodeMirror($htmlContent, {
+    fixedGutter: false,
+    //mode: mode,
+    //lineNumbers: lineNumbers,
+    lineWrapping: true,
+    tabSize: 2,
+    //lineSeparator: isWin ? "\n\r" : null, // TODO check under windows if content contains \n\r -> set
+    collapseRange: true,
+    matchBrackets: true,
+    //cursorBlinkRate: cursorBlinkRate,
+    //readOnly: isViewerMode ? "nocursor" : isViewerMode,
+    autofocus: true,
+    //theme: "lesser-dark",
+    //extraKeys: keys // workarrounded with bindGlobal plugin for mousetrap
+  });
+
+  console.debug(cmEditor);
+
+  cmEditor.on("change", function() {
+    if (contentLoaded) {
+      //TSCORE.FileOpener.setFileChanged(true);
+      console.log("cmEditor Content Changed");
+    }
+  });
+
+  cmEditor.setSize("100%", "100%");
+
+  cmEditor.setValue(content);
+  cmEditor.clearHistory();
+  cmEditor.refresh();
+
+  $htmlContent.append(cmEditor);
+}
+
+//function viewerMode(isViewerMode) {
+//
+//  cmEditor.readOnly = isViewerMode;
+//}
