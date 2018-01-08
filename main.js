b92dd0607f773c9ce441d7f7fc2e041d6e540113
kristiyandd <kristiyandd@gmail.com>
2018-01-08 16:08:07 +0200

    migration editor extensions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-/* Copyright (c) 2013-2016 The TagSpaces Authors.
+/* Copyright (c) 2013-present The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 /* globals marked, Mousetrap, CodeMirror */
-"use strict";
+'use strict';
 
 var isCordova;
 var isWin;
@@ -9,29 +9,29 @@ var isWeb;
 
 $(document).ready(function() {
   function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
       results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
   }
 
-  var locale = getParameterByName("locale");
-  var $htmlContent = $("#editorText");
+  var locale = getParameterByName('locale');
   var extSettings;
   loadExtSettings();
 
   isCordova = parent.isCordova;
   isWin = parent.isWin;
-  isWeb = parent.isWeb;
+  // isWeb = parent.isWeb;
 
   // Init internationalization
-  $.i18n.init({
-    ns: {namespaces: ['ns.editorText']},
-    debug: true,
-    lng: locale,
+  i18next.init({
+    ns: {namespaces: ['ns.editorText']} ,
+    debug: true ,
+    lng: locale ,
     fallbackLng: 'en_US'
-  }, function() {
-    $('[data-i18n]').i18n();
+  } , function() {
+    jqueryI18next.init(i18next, $);
+    $('[data-i18n]').localize();
   });
 
   if (marked) {
@@ -49,41 +49,39 @@ $(document).ready(function() {
   // Init Markdown Preview functionality
   $('#markdownPreviewModal').on('show.bs.modal', function() {
     if (marked) {
-      var modalBody = $("#markdownPreviewModal .modal-body");
+      var modalBody = $('#markdownPreviewModal .modal-body');
       modalBody.html(marked(cmEditor.getValue(), {sanitize: true}));
     } else {
-      console.log("markdown to html transformer not found");
+      console.log('markdown to html transformer not found');
     }
   });
 
-  $("#markdownPreview").on("click", function(e) {
-    $("#markdownPreviewModal").modal({show: true});
+  $('#markdownPreview').on('click', function(e) {
+    $('#markdownPreviewModal').modal({show: true});
   });
 
-  $("#mdHelpButton").on("click", function(e) {
-    $("#markdownHelpModal").modal({show: true});
+  $('#mdHelpButton').on('click', function(e) {
+    $('#markdownHelpModal').modal({show: true});
   });
 
   function handleLinks($element) {
-    $element.find("a[href]").each(function() {
-      var currentSrc = $(this).attr("href");
+    $element.find('a[href]').each(function() {
+      var currentSrc = $(this).attr('href');
       $(this).bind('click', function(e) {
         e.preventDefault();
-        var msg = {command: "openLinkExternally", link: currentSrc};
-        window.parent.postMessage(JSON.stringify(msg), "*");
+        sendMessageToHost({command: 'openLinkExternally', link: currentSrc});
       });
     });
   }
 
   function loadExtSettings() {
-    extSettings = JSON.parse(localStorage.getItem("editorTextSettings"));
+    extSettings = JSON.parse(localStorage.getItem('editorTextSettings'));
   }
 
   var filePath;
 
   function saveEditorText() {
-    var msg = {command: "saveDocument", filepath: filePath};
-    window.parent.postMessage(JSON.stringify(msg), "*");
+    sendMessageToHost({command: 'saveDocument', filepath: filePath});
   }
 
 });
@@ -112,101 +110,101 @@ function viewerMode(isViewerMode) {
 
 function setContent(content, filePath) {
 
-  var $htmlContent = $("#editorText");
-  $htmlContent.append('<div id="code" style="width: 100%; height: 100%; z-index: 9999;">');
+  var $editorText = $('#editorText');
+  $editorText.append("<div id='code' style='width: 100%; height: 100%; z-index: 9999;'>");
 
   var filetype = [];
-  filetype.h = "clike";
-  filetype.c = "clike";
-  filetype.clj = "clojure";
-  filetype.coffee = "coffeescript";
-  filetype.cpp = "clike";
-  filetype.cs = "clike";
-  filetype.css = "css";
-  filetype.groovy = "groovy";
-  filetype.haxe = "haxe";
-  filetype.htm = "xml";
-  filetype.html = "xml";
-  filetype.java = "clike";
-  filetype.js = "javascript";
-  filetype.ts = "javascript";
-  filetype.jsm = "javascript";
-  filetype.json = "javascript";
-  filetype.less = "less";
-  filetype.lua = "lua";
-  filetype.markdown = "markdown";
-  filetype.md = "markdown";
-  filetype.mdown = "markdown";
-  filetype.mdwn = "markdown";
-  filetype.mkd = "markdown";
-  filetype.ml = "ocaml";
-  filetype.mli = "ocaml";
-  filetype.pl = "perl";
-  filetype.php = "php";
-  filetype.py = "python";
-  filetype.rb = "ruby";
-  filetype.sh = "shell";
-  filetype.sql = "sql";
-  filetype.svg = "xml";
-  filetype.xml = "xml";
-  filetype.txt = "txt";
+  filetype.h = 'clike';
+  filetype.c = 'clike';
+  filetype.clj = 'clojure';
+  filetype.coffee = 'coffeescript';
+  filetype.cpp = 'clike';
+  filetype.cs = 'clike';
+  filetype.css = 'css';
+  filetype.groovy = 'groovy';
+  filetype.haxe = 'haxe';
+  filetype.htm = 'xml';
+  filetype.html = 'xml';
+  filetype.java = 'clike';
+  filetype.js = 'javascript';
+  filetype.ts = 'javascript';
+  filetype.jsm = 'javascript';
+  filetype.json = 'javascript';
+  filetype.less = 'less';
+  filetype.lua = 'lua';
+  filetype.markdown = 'markdown';
+  filetype.md = 'markdown';
+  filetype.mdown = 'markdown';
+  filetype.mdwn = 'markdown';
+  filetype.mkd = 'markdown';
+  filetype.ml = 'ocaml';
+  filetype.mli = 'ocaml';
+  filetype.pl = 'perl';
+  filetype.php = 'php';
+  filetype.py = 'python';
+  filetype.rb = 'ruby';
+  filetype.sh = 'shell';
+  filetype.sql = 'sql';
+  filetype.svg = 'xml';
+  filetype.xml = 'xml';
+  filetype.txt = 'txt';
 
-  var fileExt = filePath.substring(filePath.lastIndexOf(".") + 1, filePath.length).toLowerCase();
+  var fileExt = filePath.substring(filePath.lastIndexOf('.') + 1, filePath.length).toLowerCase();
 
   var mode = filetype[fileExt];
 
   var extensionDirectory;
   var modePath;
   if (mode) {
-    modePath = extensionDirectory + "/libs/codemirror/mode/" + mode + "/" + mode;
+    modePath = extensionDirectory + '/libs/codemirror/mode/' + mode + '/' + mode;
   }
 
   if (mode !== filetype.markdown || mode !== filetype.md ||
     mode !== filetype.mdown || mode !== filetype.mdwn) {
-    $("#markdownPreview").hide();
-    $("#mdHelpButton").hide();
+    $('#markdownPreview').hide();
+    $('#mdHelpButton').hide();
   } else {
-    $("#markdownPreview").show();
-    $("#mdHelpButton").show();
+    $('#markdownPreview').show();
+    $('#mdHelpButton').show();
   }
 
-  var place = document.getElementById("code");
+  var place = document.getElementById('code');
   cmEditor = new CodeMirror(place, {
     mode: mode,
     lineNumbers: true,
     foldGutter: true,
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
     lineWrapping: true,
     tabSize: 2,
-    //lineSeparator: isWin ? "\n\r" : null, // TODO check under windows if content contains \n\r -> set
+    //lineSeparator: isWin ? '\n\r' : null, // TODO check under windows if content contains \n\r -> set
     styleSelectedText: true,
     showCursorWhenSelecting: true,
     autofocus: !viewMode
-    //theme: "lesser-dark",
+    //theme: 'lesser-dark',
     //extraKeys: keys // workarrounded with bindGlobal plugin for mousetrap
   });
 
-  CodeMirror.modeURL = "libs/codemirror/mode/%N/%N.js";
+  CodeMirror.modeURL = 'libs/codemirror/mode/%N/%N.js';
   if (mode) {
-    cmEditor.setOption("mode", mode);
+    cmEditor.setOption('mode', mode);
     CodeMirror.autoLoadMode(cmEditor, mode);
   } else {
-    throw new TypeError("Invalid mode !");
+    throw new TypeError('Invalid mode !');
   }
 
-  CodeMirror.on(cmEditor, "change", function() {
+  CodeMirror.on(cmEditor, 'change', function() {
     if (!viewMode) {
-      var msg = {command: "contentChangedInEditor", filepath: filePath};
-      window.parent.postMessage(JSON.stringify(msg), "*");
+      var msg = {command: 'contentChangedInEditor', filepath: filePath};
+      window.parent.postMessage(JSON.stringify(msg), '*');
       $('.CodeMirror-cursor').show();
     } else {
       $('.CodeMirror-cursor').hide();
     }
   });
 
-  cmEditor.setSize("100%", "100%");
+  cmEditor.setSize('100%', '100%');
 
-  var UTF8_BOM = "\ufeff";
+  var UTF8_BOM = '\ufeff';
   if (content.indexOf(UTF8_BOM) === 0) {
     content = content.substring(1, content.length);
   }
