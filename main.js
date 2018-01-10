149e4af8788544566d9c7c18e2dfbd54106ed55a
kristiyandd <kristiyandd@gmail.com>
2018-01-10 13:38:22 +0200

    cleaned content

--- main.js
+++ main.js
@@ -9,13 +9,6 @@ var isWin;
 var isWeb;
 
 $(document).ready(function() {
-  function getParameterByName(name) {
-    name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-      results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  }
-
   var locale = getParameterByName('locale');
   initI18N(locale, 'ns.editorText.json');
 
